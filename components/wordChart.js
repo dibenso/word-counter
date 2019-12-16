026c77be33aa4ebf895dd82b8b9a601ce7ea8025
Dillon Benson <dibenso93@gmail.com>
2019-12-16 18:47:27 -0500

    show top 5 most used words in a bar chart

--- components/wordChart.js
+++ components/wordChart.js
@@ -4,43 +4,17 @@ import {
 } from 'recharts'
 import { connect } from 'react-redux'
 
-const data = [
-  {
-    name: 'Page A', uv: 4000
-  },
-  {
-    name: 'Page B', uv: 3000
-  },
-  {
-    name: 'Page C', uv: 2000
-  },
-  {
-    name: 'Page D', uv: 2780
-  },
-  {
-    name: 'Page E', uv: 1890
-  },
-  {
-    name: 'Page F', uv: 2390
-  },
-  {
-    name: 'Page G', uv: 3490
-  },
-  {
-    name: 'Page H', uv: 1890
-  },
-  {
-    name: 'Page I', uv: 2390
-  },
-  {
-    name: 'Page J', uv: 3490
-  }
-]
-
-const structureData = words => {
+const sortedWordList = words => {
   const wordList = Object.keys(words)
+  const listOfWordsWithCounts = wordList.map(word => ({word, "Word Count": words[word]}))
 
-  return wordList.map(word => ({word, count: words[word]}))
+  return [...listOfWordsWithCounts].sort((a, b) => (a["Word Count"] < b["Word Count"]) ? 1 : -1)
+}
+
+const take10HighestCounts = words => {
+  const wordList = sortedWordList(words)
+
+  return wordList.slice(0, 5)
 }
 
 const WordChart = props => {
@@ -49,14 +23,18 @@ const WordChart = props => {
   return (
     <div>
       {Object.keys(words).length > 0 && (
-        <BarChart width={500} height={300} data={structureData(words)}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="word" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="count" fill="#82ca9d" />
-        </BarChart>
+        <div>
+          <h2>Top 5 Most Used Words</h2>
+          <BarChart width={400} height={300}
+                    data={take10HighestCounts(words)}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="word" />
+            <YAxis allowDecimals={false} />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="Word Count" fill="#82ca9d" />
+          </BarChart>
+        </div>
       )}
     </div>
   )
